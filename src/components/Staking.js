5c1df1343fa2e9f27d1dca654c3a82d1ae78003a
john <jinjihoon@gmail.com>
2020-08-18 16:56:31 +0900

    first commit

---
+++ src/components/Staking.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
+
+
+const Staking = ({ address, balance }) => {
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Staking test</Form.Label>
+          <Form.Control type="email" placeholder={"test"} />
+        </Form.Group>
+          <Button variant="primary" type="submit">Staking</Button>
+          <Button variant="warning" type="submit">Unstaking</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Staking;
